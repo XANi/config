fcd969924b96fd019215392b2d41d7f30b4caef6
XANi <xani666@gmail.com>
2012-10-25 20:12:50 +0200

    typo

--- puppet/modules/xfce/manifests/init.pp
+++ puppet/modules/xfce/manifests/init.pp
@@ -2,6 +2,6 @@ define xfce::theme {
   file { "/usr/share/themes/${title}":
     ensure => directory,
     recurse => true,
-    source "$modules/xfce/files/themes/${title},
+    source "$modules/xfce/files/themes/${title}",
   }
 }
