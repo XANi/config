ab9fe852d4393ee48c3b04080a3af6e58039e1e1
Mariusz Gronczewski <xani666@gmail.com>
2012-11-03 02:07:42 +0100

    fix html mode

--- puppet/modules/emacs/files/parts/xani-html-mode.el
+++ puppet/modules/emacs/files/parts/xani-html-mode.el
@@ -2,5 +2,6 @@
 ;;
 (require 'zencoding-mode)
 (add-hook 'sgml-mode-hook 'zencoding-mode)
-
+(add-to-list 'auto-mode-alist '(".*html\\(\\|.ep\\)\\'" . sgml-mode))
+;;(add-to-list 'auto-mode-alist '("\\.\\([pP][pP]\\)\\'" . puppet-mode))
 (provide 'xani-html-mode)
