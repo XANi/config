10dc990a4e41feda4dd3a51be3ea18045c9633e8
Mariusz Gronczewski <xani666@gmail.com>
2012-11-05 14:41:13 +0100

    ecb config

---
+++ puppet/modules/emacs/files/parts/xani-ecb.el
@@ -0,0 +1,12 @@
+;;; xani-ecb.el ---
+;;
+
+(setq
+ ecb-layout-name "right1"
+ ecb-options-version "2.40"
+ ecb-primary-secondary-mouse-buttons (quote mouse-1--mouse-2)
+ ecb-windows-width 0.1
+)
+
+(provide 'xani-ecb)
+
