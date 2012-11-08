2c7423c190f1febc2054975fe33dc2ef8cb35f92
Mariusz Gronczewski <xani666@gmail.com>
2012-11-08 23:19:08 +0100

    fix ecb ver

--- puppet/modules/emacs/files/parts/xani-ecb.el
+++ puppet/modules/emacs/files/parts/xani-ecb.el
@@ -3,10 +3,14 @@
 
 (setq
  ecb-layout-name "right1"
- ecb-options-version "2.40"
  ecb-primary-secondary-mouse-buttons (quote mouse-1--mouse-2)
  ecb-windows-width 0.1
 )
+
+(custom-ser-variables
+ '(ecb-options-version "2.40")
+)
+
 
 (provide 'xani-ecb)
 
