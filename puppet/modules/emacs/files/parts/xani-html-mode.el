4f825d5c2e18f43016d63bbe5ef3b3c02ff31e9f
Mariusz Gronczewski <xani666@gmail.com>
2012-11-18 21:00:05 +0100

    fix indent on html

--- puppet/modules/emacs/files/parts/xani-html-mode.el
+++ puppet/modules/emacs/files/parts/xani-html-mode.el
@@ -4,4 +4,7 @@
 (add-hook 'sgml-mode-hook 'zencoding-mode)
 (add-to-list 'auto-mode-alist '(".*html\\(\\|.ep\\)\\'" . sgml-mode))
 ;;(add-to-list 'auto-mode-alist '("\\.\\([pP][pP]\\)\\'" . puppet-mode))
+(setq
+ sgml-basic-offset 4
+ )
 (provide 'xani-html-mode)
