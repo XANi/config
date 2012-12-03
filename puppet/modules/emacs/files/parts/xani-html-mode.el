07992e268f3c33c7f39c02db868e2cff2d86bc8a
Mariusz Gronczewski <xani666@gmail.com>
2012-12-03 15:30:28 +0100

    extension fix

--- puppet/modules/emacs/files/parts/xani-html-mode.el
+++ puppet/modules/emacs/files/parts/xani-html-mode.el
@@ -3,6 +3,7 @@
 (require 'zencoding-mode)
 (add-hook 'sgml-mode-hook 'zencoding-mode)
 (add-to-list 'auto-mode-alist '(".*html\\(\\|.ep\\)\\'" . sgml-mode))
+(add-to-list 'auto-mode-alist '(".*html\\(\\|.tt\\)\\'" . sgml-mode))
 ;;(add-to-list 'auto-mode-alist '("\\.\\([pP][pP]\\)\\'" . puppet-mode))
 (setq
  sgml-basic-offset 4
--- puppet/modules/emacs/files/parts/xani-vcl.el
+++ puppet/modules/emacs/files/parts/xani-vcl.el
@@ -1,3 +1,4 @@
+
 ;;; xani-vcl.el ---
 ;;
 
@@ -5,6 +6,6 @@
 (setq
  vcl-indent-level 4
 )
-
+(add-to-list 'auto-mode-alist '(".*vcl\\(\\|.erb\\)\\'" . vcl-mode))
 (provide 'xani-vcl)
 
