1448bb07ed914933a5b4b24766acfeff66cc8580
Mariusz Gronczewski <xani666@gmail.com>
2012-12-23 23:44:47 +0100

    snippet dir

--- puppet/modules/emacs/files/parts/xani-yasnippet.el
+++ puppet/modules/emacs/files/parts/xani-yasnippet.el
@@ -1,6 +1,7 @@
 ;;; xani-yasnippet.el ---
 ;;
 (require 'yasnippet)
-
-
+(custom-set-variables
+ '(yas/root-directory (quote ("/home/xani/emacs/yasnippet/vendor" "/home/xani/emacs/yasnippet/xani")) nil (yasnippet))
+ )
 (provide 'xani-yasnippet)
