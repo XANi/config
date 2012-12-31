ef8ba2243cda12f9fe7dd42233b52f71a4c67b7a
Mariusz Gronczewski <xani666@gmail.com>
2012-12-31 19:31:07 +0100

    clean redundant stuff

--- puppet/modules/home/manifests/config.pp
+++ puppet/modules/home/manifests/config.pp
@@ -92,9 +92,3 @@ class home::config::svn {
     }
 }
 
-define home::config::add_to_group ($group) {
-    exec { "add-${title}-to-group-${group)":
-        command => "useradd -G ${group} ${title}",
-        unless  => "id ${title} |grep -q ${group}",
-    }
-}
