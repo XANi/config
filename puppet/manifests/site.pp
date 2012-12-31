e098699066c475ede51364a2f51275102b469846
Mariusz Gronczewski <xani666@gmail.com>
2012-12-31 18:55:58 +0100

    exec defaults

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,7 +1,19 @@
+# defaults
 stage { 'init': before => Stage['pre'] }
 stage { 'pre': before => Stage['main'] }
 stage { 'post': require => Stage['main'] }
 stage { 'last': require => Stage['post'] }
+
+Exec {
+      path => [
+               '/sbin',
+               '/bin',
+               '/usr/sbin',
+               '/usr/bin',
+               '/usr/local/sbin',
+               '/usr/local/bin',
+               ],
+}
 
 $location = hiera('location','default')
 $puppet_header = "DPP/Puppet managed file at location $location, DO NOT EDIT BY HAND, changes will be overwritten."
