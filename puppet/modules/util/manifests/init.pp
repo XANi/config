4dddfac4f86064a27af9c02629e9cc4a736277a9
Mariusz Gronczewski <xani666@gmail.com>
2012-12-31 18:58:33 +0100

    utils module

---
+++ puppet/modules/util/manifests/init.pp
@@ -0,0 +1,6 @@
+define util::add_user_to_group ($user,$group) {
+    exec { "add-${user}-to-group-${group)":
+        command => "useradd -G ${group} ${user}",
+        unless  => "id ${user} |grep -q ${group}",
+    }
+}
