6eb914c03a75bf3a674dd597ae5cd4772d0110be
Mariusz Gronczewski <xani666@gmail.com>
2012-12-31 19:00:49 +0100

    fix

--- puppet/modules/util/manifests/init.pp
+++ puppet/modules/util/manifests/init.pp
@@ -1,5 +1,5 @@
 define util::add_user_to_group ($user,$group) {
-    exec { "add-${user}-to-group-${group)":
+    exec { "add-${user}-to-group-${group}":
         command => "useradd -G ${group} ${user}",
         unless  => "id ${user} |grep -q ${group}",
     }
