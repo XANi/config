f721e723cc4bf584f0c53e324f2697ad731d8119
Mariusz Gronczewski <xani666@gmail.com>
2013-01-01 07:00:40 +0100

    sepia mode off

--- puppet/modules/emacs/files/parts/xani-perl.el
+++ puppet/modules/emacs/files/parts/xani-perl.el
@@ -1,8 +1,8 @@
 ;;; xani-perl.el ---
 ;;
 
-(defalias 'perl-mode 'sepia-mode)
-(require 'sepia)
+(defalias 'perl-mode 'cperl-mode)
+;;(require 'sepia)
 (require 'perltidy)
 
 (custom-set-variables
@@ -18,11 +18,11 @@
  '(cperl-mode-hook (quote (flymake-mode)))
 
 )
-(add-to-list 'auto-mode-alist '("\\.\\([pP][Llm]\\|al\\)\\'" . sepia-mode))
-(add-to-list 'interpreter-mode-alist '("perl" . sepia-mode))
-(add-to-list 'interpreter-mode-alist '("perl5" . sepia-mode))
-(add-to-list 'interpreter-mode-alist '("miniperl" . sepia-mode))
+(add-to-list 'auto-mode-alist '("\\.\\([pP][Llm]\\|al\\)\\'" . cperl-mode))
+(add-to-list 'interpreter-mode-alist '("perl" . cperl-mode))
+(add-to-list 'interpreter-mode-alist '("perl5" . cperl-mode))
+(add-to-list 'interpreter-mode-alist '("miniperl" . cperl-mode))
 
-(define-key sepia-mode-map [backtab] 'perltidy-dwim)
+(define-key cperl-mode-map [backtab] 'perltidy-dwim)
 
 (provide 'xani-perl)
