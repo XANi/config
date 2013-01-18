21994e7c00b8ecad5562d91e55d024cee54a82f9
Mariusz Gronczewski <xani666@gmail.com>
2013-01-18 11:45:57 +0100

    no win keys

--- puppet/modules/emacs/files/parts/xani-cua.el
+++ puppet/modules/emacs/files/parts/xani-cua.el
@@ -3,6 +3,7 @@
 
 (provide 'xani-cua)
 (cua-mode t)
+(setq cua-enable-cua-keys nil) ;; we dont want windows C-v C-c C-p copy paste
 (setq cua-auto-tabify-rectangles nil) ;; Don't tabify after rectangle commands
 (transient-mark-mode 1) ;; No region when it is not highlighted
 (setq cua-keep-region-after-copy t)
