05bb6ca3fdfbbc2d7f074c7db717eb7786922dca
Mariusz Gronczewski <xani666@gmail.com>
2013-01-18 11:03:21 +0100

    fix

--- puppet/modules/emacs/files/parts/xani-cua.el
+++ puppet/modules/emacs/files/parts/xani-cua.el
@@ -2,3 +2,7 @@
 ;;
 
 (provide 'xani-cua)
+(cua-mode t)
+(setq cua-auto-tabify-rectangles nil) ;; Don't tabify after rectangle commands
+(transient-mark-mode 1) ;; No region when it is not highlighted
+(setq cua-keep-region-after-copy t)
