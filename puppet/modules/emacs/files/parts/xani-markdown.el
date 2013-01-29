26d47d169e7844600c33e43114170bd20f9197a4
XANi <mgronczewski@artegence.com>
2013-01-29 17:33:38 +0100

    open .md with markdown

---
+++ puppet/modules/emacs/files/parts/xani-markdown.el
@@ -0,0 +1,9 @@
+;;; xani-markdown.el ---
+;;
+
+(provide 'xani-markdown)
+ (add-to-list 'auto-mode-alist '("\\.\\([mM][dD]\\)\\'" . cperl-mode))
+(setq
+ markdown-enable-math t
+ markdown-uri-types (quote ("acap" "cid" "data" "dav" "fax" "file" "ftp" "gopher" "git" "http" "https" "imap" "ldap" "mailto" "mid" "modem" "news" "nfs" "nntp" "pop" "prospero" "rtsp" "service" "sip" "ssh" "tel" "telnet" "tip" "urn" "vemmi" "wais"))
+)
