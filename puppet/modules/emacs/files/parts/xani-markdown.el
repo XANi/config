81545bc9e00297c24c71e02cd311077e917997b0
XANi <mgronczewski@artegence.com>
2013-01-30 12:46:01 +0100

    fail

--- puppet/modules/emacs/files/parts/xani-markdown.el
+++ puppet/modules/emacs/files/parts/xani-markdown.el
@@ -2,7 +2,7 @@
 ;;
 
 (provide 'xani-markdown)
- (add-to-list 'auto-mode-alist '("\\.\\([mM][dD]\\)\\'" . cperl-mode))
+ (add-to-list 'auto-mode-alist '("\\.\\([mM][dD]\\)\\'" . markdown-mode))
 (setq
  markdown-enable-math t
  markdown-uri-types (quote ("acap" "cid" "data" "dav" "fax" "file" "ftp" "gopher" "git" "http" "https" "imap" "ldap" "mailto" "mid" "modem" "news" "nfs" "nntp" "pop" "prospero" "rtsp" "service" "sip" "ssh" "tel" "telnet" "tip" "urn" "vemmi" "wais"))
