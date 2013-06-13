c7cab2966e48a735130d6f4bfe9b04fbaa3f31bb
XANi <mgronczewski@artegence.com>
2013-06-13 13:04:58 +0200

    human readable buffer size

--- modules/emacs/files/parts/xani-ibuffer.el
+++ modules/emacs/files/parts/xani-ibuffer.el
@@ -4,10 +4,16 @@
 ;; use ibuffer by default
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 (autoload 'ibuffer "ibuffer" "List buffers." t)
+(define-ibuffer-column size-h
+  (:name "Size" :inline t)
+  (cond
+   ((> (buffer-size) 1000) (format "%7.1fk" (/ (buffer-size) 1000.0)))
+   ((> (buffer-size) 1000000) (format "%7.1fM" (/ (buffer-size) 1000000.0)))
+   (t (format "%8d" (buffer-size)))))
 (setq ibuffer-formats (
                        quote ((mark modified read-only " "
                                     (name 40 40 :left :elide) " "
-                                    (size 9 -1 :right) " "
+                                    (size-h 9 -1 :right) " "
                                     (mode 16 16 :left :elide) " "
                                     filename-and-process
                                     )
