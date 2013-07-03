412e355f024a5f37d45f9faac5653f09e9324035
XANi <mgronczewski@artegence.com>
2013-07-03 14:58:49 +0200

    new snippets

--- modules/emacs/files/parts/xani-yasnippet.el
+++ modules/emacs/files/parts/xani-yasnippet.el
@@ -1,31 +1,36 @@
 ;;; xani-yasnippet.el ---
 ;;
-(require 'yasnippet)
-
 ;; new snippets go to first dir on list
-(setq yas/root-directory
-      '(
-        "/home/xani/emacs/yasnippet/custom"
-        "/home/xani/emacs/yasnippet/import"
-        "/home/xani/emacs/yasnippet/xani"
-        "/home/xani/emacs/yasnippet/vendor"
+(custom-set-variables
+ '(yas-global-mode t nil (yasnippet))
+ '(yas-snippet-dirs
+   (quote
+    (
+     "/home/xani/emacs/yasnippet/custom"
+     "/home/xani/emacs/yasnippet/import"
+     "/home/xani/emacs/yasnippet/xani"
+     "/home/xani/emacs/yasnippet/vendor"
+    ) ))
+ )
+(require 'yasnippet)
+(setq
+ hippie-expand-try-functions-list
+ (
+  quote (
+         yas/hippie-try-expand
+         try-complete-file-name-partially
+         try-complete-file-name
+         try-expand-all-abbrevs
+         try-expand-list
+         try-expand-line
+         try-expand-dabbrev
+         try-expand-dabbrev-all-buffers
+         try-expand-dabbrev-from-kill
+         try-complete-lisp-symbol-partially
+         try-complete-lisp-symbol
+              )
         )
-
-      hippie-expand-try-functions-list (quote (
-                                               yas/hippie-try-expand
-                                               try-complete-file-name-partially
-                                               try-complete-file-name
-                                               try-expand-all-abbrevs
-                                               try-expand-list
-                                               try-expand-line
-                                               try-expand-dabbrev
-                                               try-expand-dabbrev-all-buffers
-                                               try-expand-dabbrev-from-kill
-                                               try-complete-lisp-symbol-partially
-                                               try-complete-lisp-symbol
-                                               )
-                                              )
 )
-(yas/initialize)
+
 (mapc 'yas/load-directory yas/root-directory)
 (provide 'xani-yasnippet)
