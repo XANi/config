34fdf30fbd2f40a3b7e1b4f4a513d0d7595fa328
XANi <mgronczewski@artegence.com>
2013-07-17 16:28:54 +0200

    fix, that shoudl be string not array for org-mode to interpret it as file containing list of files

--- modules/emacs/files/parts/xani-org-mode-arte.el
+++ modules/emacs/files/parts/xani-org-mode-arte.el
@@ -4,7 +4,7 @@
 (setq
  org-todo-keywords '((type "TODO(t!)" "NEXT(n!)" "WAIT(w!)" "REOPEN(r!)" "|" "DONE(d!)" "CANCELLED(c@)" "MERGED(m@)"))
  org-directory "~/emacs/org/arte"
- org-agenda-files '("~/emacs/org/arte/.agenda")
+ org-agenda-files "~/emacs/org/arte/.agenda"
  ;; org-mobile-files '("~/emacs/org/arte/.agenda") ;; defaults to agenda
  org-mobile-directory "~/emacs/org/mobile.org/arte"
  org-mobile-inbox-for-pull "~/emacs/org/arte/mobile.org"
