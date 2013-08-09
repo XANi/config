fec53c8f0bf2eaeeabb067b875795bae2250e721
XANi <mgronczewski@efigence.com>
2013-08-09 13:08:10 +0200

    udev config for yubikey

--- modules/token/manifests/init.pp
+++ modules/token/manifests/init.pp
@@ -1,6 +1,6 @@
 class token {
     util::add_user_to_group {
-        'xani-scard':
+        'xani-pcscd':
             user    => 'xani',
             group   => 'pcscd',
             require => Package['pcscd'],
@@ -26,6 +26,11 @@ class token {
     file {'/usr/local/bin/token-agent.sh':
         content => template('token/token-agent.sh.erb'),
         mode    => 755,
+        owner   => root,
+    }
+    file {'/etc/udev/rules.d/99-yubikey.rules':
+        content => template('token/yubikey.udev'),
+        mode    => 644,
         owner   => root,
     }
 }
