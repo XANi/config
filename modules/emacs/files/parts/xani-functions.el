205bbec7d8305a6f7e17421742a5e3b4e8b4cbaa
XANi <mgronczewski@efigence.com>
2013-08-22 12:45:36 +0200

    fix

--- modules/emacs/files/parts/xani-functions.el
+++ modules/emacs/files/parts/xani-functions.el
@@ -3,13 +3,11 @@
 
 ;; a bunch of random functions, mostly stolen from random parts of internet
 
-(defun volatile-kill-buffer ()
-  "Kill current buffer unconditionally."
-  (interactive)
-  (let ((buffer-modified-p nil))
-    (kill-buffer (current-buffer))
-    )
+
+(defun volatile-kill-buffer (&optional buffer)
+    "Kill current buffer, even if it has been modified."
+    (interactive)
+    (set-buffer-modified-p nil)
+    (if buffer (kill-buffer buffer) (kill-buffer))
 )
-
-
 (provide 'xani-functions)
