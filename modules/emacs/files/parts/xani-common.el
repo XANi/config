bd0b706a2f39e33be364293a4419a662751ab96a
XANi <mgronczewski@efigence.com>
2013-09-11 14:31:04 +0200

    fix faces

--- modules/emacs/files/parts/xani-common.el
+++ modules/emacs/files/parts/xani-common.el
@@ -81,10 +81,6 @@
  )
 
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(indent-tabs-mode nil)
  '(standard-indent 4)
  '(tab-width 4)
