0b52f62cbc2f261159e2266ac71cac4ddd160694
Mariusz Gronczewski <xani666@gmail.com>
2013-09-25 13:00:35 +0200

    nyanyanyanyanfixnyanyanyan

--- modules/emacs/files/parts/xani-nyan.el
+++ modules/emacs/files/parts/xani-nyan.el
@@ -1,12 +1,12 @@
 ;;; xani-nyan.el ---
 ;;
+(nyan-mode)
 (setq
  nyan-animate-nyancat t
  nyan-bar-length 16
- nyan-wavy-trail nil
+ nyan-wavy-trail t
  nyan-mode t
 )
-
-
+(nyan-start-animation)
 
 (provide 'xani-nyan)
