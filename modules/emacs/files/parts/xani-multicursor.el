a7842b857c160739cbadcdcd935f4be29c3452bd
Mariusz Gronczewski <xani666@gmail.com>
2013-09-30 22:01:49 +0200

    nest into url key for easier parsing

--- modules/emacs/files/parts/xani-multicursor.el
+++ modules/emacs/files/parts/xani-multicursor.el
@@ -1,7 +1,8 @@
 ;;; xani-multicursor.el ---
 ;;
 ;; https://github.com/magnars/multiple-cursors.el
-(global-set-key (kbd "C-S-<mouse-1>") 'mc/add-cursor-on-click)
+(global-unset-key (kbd "S-<mouse-1>"))
+(global-set-key (kbd "S-<down-mouse-1>") 'mc/add-cursor-on-click)
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-M->") 'mc/mark-all-like-this)
--- modules/emacs/files/parts/xani-ruby.el
+++ modules/emacs/files/parts/xani-ruby.el
@@ -1,7 +1,18 @@
 ;;; xani-ruby.el ---
 ;;
 (setq
- ruby-indent-level 4 ;; default ruby indent level is unreadable
+ ruby-indent-level 2
  ruby-insert-encoding-magic-comment nil
 )
+(defun efi-ruby-mode ()
+  "Ruby mode with adjusted indent"
+  (interactive)
+  (ruby-mode)
+  (setq-local
+   ruby-indent-level 4 ;; 2 indent is ugly
+   )
+ )
+(add-to-list 'auto-mode-alist '(".*/src/puppet.*\\.\\(rb\\)\\'" . efi-ruby-mode))
+
+
 (provide 'xani-ruby)
