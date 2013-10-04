95d94ee9e9e4f180857e068ced90b7df5b55812f
Mariusz Gronczewski <xani666@gmail.com>
2013-10-04 16:31:04 +0200

    rename work host

--- modules/emacs/manifests/init.pp
+++ modules/emacs/manifests/init.pp
@@ -371,7 +371,7 @@ class emacs::org::sync ( $homedir = '/home/xani' ) {
         unless    => "/usr/bin/sudo -u xani /usr/bin/test -e $homedir/emacs/org/mobile.org/remote", # fuse works in a way that forbids root to have access to user-mounted files, so we sudo to mounting user
         logoutput => true,
         command   => $hostname ? {
-            'spare2' => "/usr/bin/sudo -u xani -i /usr/bin/tsocks /usr/bin/sshfs orgmode@devrandom.eu:/home/orgmode $homedir/emacs/org/mobile.org/",
+            'ghroth' => "/usr/bin/sudo -u xani -i /usr/bin/tsocks /usr/bin/sshfs orgmode@devrandom.eu:/home/orgmode $homedir/emacs/org/mobile.org/",
             default  => "/usr/bin/sudo -u xani -i /usr/bin/sshfs orgmode@devrandom.eu:/home/orgmode $homedir/emacs/org/mobile.org/",
         },
         require   => [Package['sshfs'], Exec['xani-emacs-mobile-org'],]
