4f2d4be4002821d5541e7b95ea73bfd2766f0cbe
Mariusz Gronczewski <xani666@gmail.com>
2013-12-06 12:17:09 +0100

    fix puppet

--- modules/emacs/files/parts/xani-puppet.el
+++ modules/emacs/files/parts/xani-puppet.el
@@ -8,10 +8,10 @@
 (add-hook 'puppet-mode-hook (lambda () (flymake-puppet-load)))
 (add-to-list 'auto-mode-alist '("\\.\\([pP][pP]\\)\\'" . puppet-mode))
 
-(custom-set-variables
- '(puppet-comment-column 64)
- '(puppet-include-indent 4)
- '(puppet-indent-level 4)
+(setq
+ puppet-comment-column 64
+ puppet-include-indent 4
+ puppet-indent-level 4
+ flymake-puppet-executable "puppet-lint-emacs"
 )
-
 (provide 'xani-puppet)
