1871ac0edb83bbc881a6649c9dd6e519e57c50f4
Mariusz Gronczewski <xani666@gmail.com>
2013-12-17 12:36:15 +0100

    fix agendas

--- modules/emacs/files/parts/xani-org-mode-arte.el
+++ modules/emacs/files/parts/xani-org-mode-arte.el
@@ -8,6 +8,7 @@
  ;; org-mobile-files '("~/emacs/org/arte/.agenda") ;; defaults to agenda
  org-mobile-directory "~/emacs/org/mobile.org/arte"
  org-mobile-inbox-for-pull "~/emacs/org/arte/mobile.org"
+ org-mobile-agendas (quote default)
  org-combined-agenda-icalendar-file "~/emacs/org/arte.ics"
 )
 
