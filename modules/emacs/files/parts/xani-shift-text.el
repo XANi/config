43468c040cf9773e8c8feced644b247d048bd1e3
Mariusz Gronczewski <xani666@gmail.com>
2014-02-26 10:40:30 +0100

    shift text

---
+++ modules/emacs/files/parts/xani-shift-text.el
@@ -0,0 +1,11 @@
+;;; xani-shift-text.el ---
+;;
+
+(global-set-key (kbd "<M-s-up>")     'shift-text-up)
+(global-set-key (kbd "<M-s-down>")   'shift-text-down)
+(global-set-key (kbd "<M-s-left>")   'shift-text-left)
+(global-set-key (kbd "<M-s-right>")  'shift-text-right)
+
+
+
+(provide 'xani-shift-text)
