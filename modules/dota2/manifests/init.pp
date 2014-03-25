cd6a7a141cba2de5dadbfd601b4c9407dcfac728
Mariusz Gronczewski <xani666@gmail.com>
2014-03-26 00:25:54 +0100

    remove alttab guides

--- modules/dota2/manifests/init.pp
+++ modules/dota2/manifests/init.pp
@@ -1,7 +1,6 @@
 class dota2 {
     include dota2::vars
     include dota2::autoexec
-    include dota2::itembuilds
 }
 
 
@@ -11,18 +10,14 @@ class dota2::vars {
     $dota_user = 'xani'
 }
 
-class dota2::itembuilds {
-    include dota2
-    $dotadir = $dota2::vars::dotadir
-    $dotauser = $dota2::vars::dota_user
-    file { "${dotadir}/dota/itembuilds/":
-        ensure => directory,
-        source => 'puppet:///modules/dota2/itembuilds',
-        recurse => true,
-        owner  => $dotauser
-    }
-}
 class dota2::autoexec {
-    notify {"Autoexec goes here":;}
+    require dota2::vars
+    $basedir = $dota2::vars::dotadir
+    file { "${basedir}//dota/cfg/autoexec.cfg":
+        content => template('dota/autoexec.cfg'),
+        owner   => xani,
+        mode    => 644,
+    }
+
 
 }
