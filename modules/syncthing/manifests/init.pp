05c5094a5d97425348581e545bdcc9b4d0ffa43a
Mariusz Gronczewski <xani666@gmail.com>
2015-07-06 12:30:07 +0200

    dep fix

--- modules/syncthing/manifests/init.pp
+++ modules/syncthing/manifests/init.pp
@@ -11,11 +11,12 @@ class syncthing  {
 
 define syncthing::instance {
     include syncthing
-    file {"/etc/systemd/system/syncthing-${title}":
+    file {"/etc/systemd/system/syncthing-${title}.service":
         content => template('syncthing/systemd'),
         mode => 644,
     }
     service {"syncthing-${title}":
         enable => true,
+        require => File["/etc/systemd/system/syncthing-${title}"]
     }
 }
