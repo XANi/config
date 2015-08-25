feb5dd7f75237b628b8f30869efd2daed92b8854
Mariusz Gronczewski <xani666@gmail.com>
2015-08-25 14:30:33 +0200

    burn it with napalm

--- modules/home/manifests/tweaks.pp
+++ modules/home/manifests/tweaks.pp
@@ -14,6 +14,7 @@ class home::tweaks {
                backup => false,
                purge  => true,
                recurse => true,
+               force  => true,
                ensure => directory,
     }
 
