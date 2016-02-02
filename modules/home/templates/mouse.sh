8e2843d02c82f751d7619059e610aa7b1e2a0900
Mariusz Gronczewski <xani666@gmail.com>
2016-02-02 22:06:05 +0100

    mouse

--- modules/home/templates/mouse.sh
+++ modules/home/templates/mouse.sh
@@ -1,5 +1,4 @@
-xinput set-button-map 9 1 2 3 4 5 6 7 8 2
-xinput set-button-map 10 1 2 3 4 5 6 7 8 2
+#xinput set-button-map 9 1 2 3 4 5 6 7 8 2
+#xinput set-button-map 10 1 2 3 4 5 6 7 8 2
 xinput set-prop 9 'Device Accel Profile' -1
 xinput set-prop 10 'Device Accel Profile' -1
-
