37006a5ec669c403887648eec05536b7c8ced4f8
Mariusz Gronczewski <xani666@gmail.com>
2016-03-28 04:07:07 +0200

    fuck everything about package management in emacs, they cant even get tls working

--- modules/emacs/templates/install-packages.el
+++ modules/emacs/templates/install-packages.el
@@ -4,22 +4,13 @@
 ;; gnutls needs that for domains with fucked up SSL (no intermediates)
 ;; you'd need to fire gnutls-cli --tofu marmalade-repo.org once to make it trusted
 ;; tofu means "trust on first usage"
-(setq tls-program '("gnutls-cli --tofu -p %p %h"))
+(setq tls-program '("gnutls-cli --no-ca-verification -p %p %h"))
 
 
-;;(setq package-archives '(
-;;    ("ELPA" . "http://tromey.com/elpa/")
-;;   ("gnu" . "http://elpa.gnu.org/packages/")
-;;    ("melpa" . "http://melpa.milkbox.net/packages/")
-;;    ("marmalade" . "http://marmalade-repo.org/packages/")))
-(require 'package)
-(add-to-list 'package-archives
-         '("marmalade" . "https://marmalade-repo.org/packages/")
-         '("melpa" . "http://melpa.milkbox.net/packages/")
-	 '("gnu" . "http://elpa.gnu.org/packages/")
-	 '("ELPA" . "http://tromey.com/elpa/")
-)
-		
+
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+                         ("marmalade" . "https://marmalade-repo.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
 (package-initialize)
 (package-refresh-contents)
 (setq package-list '(
