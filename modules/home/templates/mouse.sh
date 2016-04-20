28dad220cb3517f34496623ce31175c0c019e0a3
Mariusz Gronczewski <xani666@gmail.com>
2016-04-20 09:50:09 +0200

    crude autodetection of right device

--- modules/home/templates/mouse.sh
+++ modules/home/templates/mouse.sh
@@ -1,4 +1,12 @@
+#!/usr/bin/perl
+open(DEVLIST,'-|','xinput');
+while(<DEVLIST>) {
+    if ($_ =~ /id=(\d+)/) {
+        my $dev_id=$1;
+        if ($_ =~ /pointer/ && $_ !~ /keyboard/i && $dev_id > 4) {
+            system('xinput', 'set-prop', $dev_id, 'Device Accel Profile', '-1');
+        }
+    }
+}
 #xinput set-button-map 9 1 2 3 4 5 6 7 8 2
 #xinput set-button-map 10 1 2 3 4 5 6 7 8 2
-xinput set-prop 9 'Device Accel Profile' -1
-xinput set-prop 10 'Device Accel Profile' -1
