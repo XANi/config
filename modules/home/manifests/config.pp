abe5116207cccc851bf5f534d50af6e73ec52ffa
Mariusz Gronczewski <xani666@gmail.com>
2016-07-05 08:06:31 +0200

    fix ?

--- modules/home/manifests/config.pp
+++ modules/home/manifests/config.pp
@@ -12,7 +12,7 @@ class home::config (
         owner => xani,
         group => xani,
     }
-    $git = $home::common::git
+    $git = hiera_hash('git'),
     home::config::file {
         bash_prompt:;
         bash_functions:;
