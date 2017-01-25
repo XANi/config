976cf1a2069d89844c9ab26d1f4608b81c4a9d5d
Mariusz Gronczewski <xani666@gmail.com>
2017-01-25 09:37:48 +0100

    dont dabbrev, it is pretty much useless

--- modules/emacs/files/parts/xani-company.el
+++ modules/emacs/files/parts/xani-company.el
@@ -16,5 +16,16 @@
 (setq company-abort-manual-when-too-short t)
 (setq company-go-show-annotation t)
 ;;(setq company-tooltip-align-annotations t)
+ ;; '(company-backends
+ ;;  (quote
+ ;;   (company-bbdb company-nxml company-css company-eclim company-semantic company-clang company-xcode company-cmake company-capf company-files
+ ;;                 (company-dabbrev-code company-gtags company-etags company-keywords)
+ ;;                 company-oddmuse company-dabbrev)))
+
+(setq company-backends
+   (quote
+    (company-bbdb company-nxml company-css company-eclim company-semantic company-clang company-xcode company-cmake company-capf
+                  (company-gtags company-etags company-keywords)
+                  company-oddmuse)))
 
 (provide 'xani-company)
