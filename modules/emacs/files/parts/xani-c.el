8ee6705ecad6dc940549f3afb9ffa0793c7a4385
Mariusz Gronczewski <xani666@gmail.com>
2017-09-24 21:25:37 +0200

    fix C mode

--- modules/emacs/files/parts/xani-c.el
+++ modules/emacs/files/parts/xani-c.el
@@ -8,14 +8,15 @@
 
 (defun linux-c-mode ()
 "C mode with adjusted defaults for use with the Linux kernel."
-(interactive)
-(c-mode)
-(c-set-style "K&R")
-(setq tab-width 8)
-(setq indent-tabs-mode t)
-(setq c-basic-offset 8))
+  (interactive)
+  (c-mode)
+  (c-set-style "K&R")
+  (whitespace-mode)
+  (setq tab-width 8)
+  (setq indent-tabs-mode t)
+  (setq c-basic-offset 8))
 
-(setq auto-mode-alist (cons '("*.c" . linux-c-mode)
-                            auto-mode-alist))
-
+;(setq auto-mode-alist (append '(("\\.c$" . linux-c-mode))
+;      auto-mode-alist))
+(add-to-list 'auto-mode-alist '("\\.c$" . linux-c-mode))
 (provide 'xani-c)
